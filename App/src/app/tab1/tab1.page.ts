2d7ac2b3e4800c18e31066a717cc2f9c3fb67607
Ronan-Yann Lorin <ryl@free.fr>
2020-05-12 22:49:12 +0200

    some testing / fixing

--- App/src/app/tab1/tab1.page.ts
+++ App/src/app/tab1/tab1.page.ts
@@ -1,81 +1,69 @@
-
-
-
 import { Component } from '@angular/core';
-import { Plugins, CameraResultType, CameraSource, FilesystemDirectory, FilesystemEncoding} from '@capacitor/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
 import { Router, NavigationExtras } from '@angular/router';
 
 const { Filesystem } = Plugins;
 
-
-
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  private readonly baseUrl = 'http://localhost:3000';
-  // 'http://vps183484.vps.ovh.ca:3000';
-  photo: SafeResourceUrl;
+//	private readonly baseUrl = 'http://localhost:3000';
+	private readonly baseUrl = 'http://vps183484.vps.ovh.ca:3000';
+	photo: SafeResourceUrl;
 
+	constructor(
+		private sanitizer: DomSanitizer,
+		private router: Router,
+		private httpClient: HttpClient) {
+		this.photo = '/assets/images/000000-0.1.png'; // initialize with blank photo
+	}
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    private router: Router,
-    private httpClient: HttpClient) {
-    this.photo = '/assets/images/000000-0.1.png';
-  }
+	async uploadImages(contents: any) {
+		console.log('uploadImages/contents: ' + contents.webPath);
+		const formData = new FormData();
 
+		const response = fetch(contents.webPath);
+		const blob = await (await response).blob();
 
-  //  uploadImages(contents): Promise<any[]> {
-  async uploadImages(contents: any) {
-    console.log('uploadImages/contents: ' + contents.webPath);
-    const formData = new FormData();
+		console.log('uploadImages/typeof blob: ' + typeof blob);
+		console.log('uploadImages/blob: ' + blob);
 
-    const response = fetch(contents.webPath);
-    const blob = await (await response).blob();
+		formData.set('photos', blob, contents.webPath);
 
-    console.log('uploadImages/typeof blob: ' + typeof blob);
-    console.log('uploadImages/blob: ' + blob);
+		console.log('uploadImages/this.httpClient.post ' + `${this.baseUrl}/list-images`);
+		this.httpClient.post(`${this.baseUrl}/upload-photos`, formData).subscribe((value) => {
+			console.log('uploadImages/post result: ' + value[0].newName);
+			const extras: NavigationExtras = {
+				queryParams: {
+					special: value[0].newName
+				}
+			};
+			this.router.navigate(['/tabs/tab2'], extras);
+		}
+		);
+		this.httpClient.get(`${this.baseUrl}/list-images`).subscribe((value) => console.log('uploadImages/get result: ' + value));
+		console.log('uploadImages/after post');
+	}
 
-    formData.set('photos', blob, contents.webPath);
+	async takePhoto() {
+		const image = await Plugins.Camera.getPhoto({
+			quality: 100,
+			allowEditing: false,
+			resultType: CameraResultType.Uri,
+			source: CameraSource.Prompt
+		});
 
-    console.log('uploadImages/this.httpClient.post');
-    this.httpClient.get(`${this.baseUrl}/list-images`).subscribe((value) => console.log('uploadImages/get result: ' + value));
-
-    this.httpClient.post(`${this.baseUrl}/upload-photos`, formData).subscribe((value) => {
-      console.log('uploadImages/post result: ' + value[0].newName);
-      const extras: NavigationExtras = {
-        queryParams: {
-          special: value
-        }
-      };
-      this.router.navigate(['/tabs/tab2'], extras);
-    }
-      );
-
-    console.log('uploadImages/after post');
-
-    //  return this.httpClient.post<any[]>(`${this.baseUrl}/upload-photos`, formData).toPromise();
-  }
-
-  async takePhoto() {
-    const image = await Plugins.Camera.getPhoto({
-      quality: 100,
-      allowEditing: false,
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Prompt
-    });
-
-    console.log('takePhoto/image.dataUrl: ' + image.dataUrl); // data:image/jpeg;base64,...
-    console.log('takePhoto/image.path: ' + image.path); //  file:///data/user/0/com.rylorin.camapp/cache/JPEG_...
-    console.log('takePhoto/image.webPath: ' + image.webPath);
-    //  http://localhost/_capacitor_file_/data/user/0/com.rylorin.camapp/cache/JPEG_...
-    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.webPath));
-    this.uploadImages(image);
-  }
+		console.log('takePhoto/image.dataUrl: ' + image.dataUrl); // data:image/jpeg;base64,...
+		console.log('takePhoto/image.path: ' + image.path); //  file:///data/user/0/com.rylorin.camapp/cache/JPEG_...
+		console.log('takePhoto/image.webPath: ' + image.webPath);
+		//  http://localhost/_capacitor_file_/data/user/0/com.rylorin.camapp/cache/JPEG_...
+		this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.webPath));
+		this.uploadImages(image);
+	}
 
 }
--- App/src/app/tab2/tab2.page.ts
+++ App/src/app/tab2/tab2.page.ts
@@ -1,37 +1,32 @@
-
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
-
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+	selector: 'app-tab2',
+	templateUrl: 'tab2.page.html',
+	styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit,AfterViewInit {
 
-  private readonly baseUrl = 'http://localhost:3000';
-  // tslint:disable-next-line: ban-types
-  return: Object;
-  tester: any;
+export class Tab2Page implements OnInit, AfterViewInit {
+	// tslint:disable-next-line: ban-types
+	return: Object;
+	tester: any;
 
-  constructor(/*private httpClient: HttpClient*/ private route: ActivatedRoute, private router: Router) {
-    this.route.queryParams.subscribe(params => {
-      if (params && params.special){
-        this.tester = params.special;
-      }
-    });
-  }
+	constructor(private route: ActivatedRoute, private router: Router) {
+		this.route.queryParams.subscribe(params => {
+			if (params && params.special) {
+				console.log('tab2: ' + params.special);
+				this.tester = params.special;
+			}
+		});
+	}
 
-  ngOnInit() {
+	ngOnInit() {
 
+	}
 
-  }
-  ngAfterViewInit(){
-    // console.log(this.baseUrl);
-    // this.httpClient.get(`${this.baseUrl}/list-images`).subscribe((value) => this.return = value);
-  }
+	ngAfterViewInit() {
+
+	}
 
 }
