482779e9e84e6a76775dc1d23388bc9d39eaf538
Lepater <antoine.lorin2@gmail.com>
2020-05-14 15:35:19 +0200

    Add files via upload

--- App/src/app/tab2/tab2.page.ts
+++ App/src/app/tab2/tab2.page.ts
@@ -1,32 +1,47 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { Component, OnInit, AfterViewInit, ViewEncapsulation } from '@angular/core';
+
 import { ActivatedRoute, Router } from '@angular/router';
 
+
+
+
+
 @Component({
-	selector: 'app-tab2',
-	templateUrl: 'tab2.page.html',
-	styleUrls: ['tab2.page.scss']
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-
 export class Tab2Page implements OnInit, AfterViewInit {
-	// tslint:disable-next-line: ban-types
-	return: Object;
-	tester: any;
 
-	constructor(private route: ActivatedRoute, private router: Router) {
-		this.route.queryParams.subscribe(params => {
-			if (params && params.special) {
-				console.log('tab2: ' + params.special);
-				this.tester = params.special;
-			}
-		});
-	}
+  private readonly baseUrl = 'http://localhost:3000';
+  // tslint:disable-next-line: ban-types
+  return: any;
 
-	ngOnInit() {
+  display = 0;
 
-	}
 
-	ngAfterViewInit() {
 
-	}
+  // tslint:disable-next-line: max-line-length
+  constructor(/*private httpClient: HttpClient*/ private route: ActivatedRoute, private router: Router, private domSanitizer: DomSanitizer) {
+    this.route.queryParams.subscribe(params => {
+      if (params && params.special) {
+        console.log('Special Param : ' + params.special);
+        this.return = this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:3000/files?file=' + params.special);
+        console.log(this.return);
+        this.display = 1;
+
+      }
+    });
+  }
+
+  ngOnInit() {
+
+  }
+  ngAfterViewInit() {
+     // this.httpClient.get(`${this.baseUrl}/list-images`).subscribe((value) => this.return = value);
+  }
 
 }
